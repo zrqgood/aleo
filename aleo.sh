30bd38b2fa29fb7409b97a77912d21a8d7113f62
zrqgood <zrqgood@yeah.net>
2024-09-15 23:36:57 +0800

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -3,5 +3,5 @@ wget  https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/al
 apt install unzip
 unzip aleominer+3.0.3.zip
 chmod +x aleo_setup.sh
-sudo ./aleo_setup.sh -p stratum+tcp://aleo-asia.f2pool.com:4400 -w zrqgood.customWork1
+sudo ./aleo_setup.sh -p stratum+tcp://195.201.170.121:3030 -w zrqgood.customWork1
 tail -f prover.log
